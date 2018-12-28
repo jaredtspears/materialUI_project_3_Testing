bbc052672eadb14a06547849f39e0ea76514afc3
Ha1f_Deaf <jaredtspears@gmail.com>
2018-12-28 11:25:03 -0500

    added structure to the files, went off much of the structure of activity 11. NavTabs.js and pages are not working yet. Do not have API set up yet, did install Axios.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react';
-// import Card from '../src/components/Card/card';
-import NavBar from './components/NavBar/navBar';
-import './App.css';
+import React from 'react';
+// inital login card:
+import Card from "./components/LoginPgComponents/Card/card";
+// navbar will hold the NavTabs listed below for main collections 
+//overview page( the homepage):
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Login from "./components/pages/login";
-import Profile from "./components/pages/profile";
-import Collections from "./components/pages/collection";
-import About from "./components/pages/about";
+import NavBar from './components/MainPgComponents/NavBar/navBar';
+import NavTabs from "./components/MainPgComponents/NavTabs";
+import loginPg from "./pages/loginPg";
+import collectionsPg from "./pages/collectionsPg";
+import mainPg from "./pages/mainPg";
+import './App.css';
 
-class App extends Component {
-  render() {
+// changed from stateful to stateless component Statefulness will be rendered like that in activity 11 in 
+//react activities. Ours should be stateful possibly in the pages of collectionsPg.js and mainPg.js 
+function App() {
     return (
       <div>
         <NavBar>
           <Router>
             <div>
               <NavTabs />
-              <Route exact path="/" component={Login} />
-              <Route exact path="/myprofile" component={Profile} />
-              <Route exact path="/collections" component={Collections} />
-              <Route path="/about" component={About} />
+              <Route exact path="/" component= {mainPg} />
+              <Route exact path="/login" component={loginPg} />
+              <Route exact path="/collections" component={collectionsPg} />
+              {/* <Route path="/about" component={About} /> */}
             </div>
           </Router>
         </NavBar>
+
+        {/* <Card/> below card should be the initial loginPg route and
+        all other components should be disabled or hidden until a user
+        either logins or creates an acct. This is only for testing on 
+        what it will look like, please delete when route bove is working */}
+        <Card/>
      </div>
     );
   }
-}
+
 
 export default App;
